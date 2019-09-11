263b6da9008b3800385efff1ecd56b8341938d42
Andriy Bench <andriy.bench@gmail.com>
2019-09-11 09:15:38 +0300

    Added reading operation just after write operation

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-uint32 delay=1000;
+uint32 delay=20000;
 char buffer[80];
 
 void print_error_status(uint32 error_status)
@@ -58,13 +58,15 @@ int main(void)
     UART_PORT_Start();
     I2C_PORT_Start();
     uint32 address=0b0101100; //chip 0, 7 bit 
-    uint32 data_length=3;
-    uint8 wr_buffer[3]={0b00000001, // RDAC1
-        //0b00000011,// RDAC3
+    //uint32 rd_address=0b0101100;
+    uint32 data_length=2;
+    uint8 wr_buffer[3]={//0b00000001, // RDAC1
+        0b00000011,// RDAC3
         
         0b00000000,
         0b00000000,
     };
+    uint8 rd_buffer[3] = {1,1,1};
     
     uint32 err = 0;
 
@@ -78,7 +80,14 @@ int main(void)
         err = I2C_PORT_I2CMasterWriteBuf(address,wr_buffer, data_length, I2C_PORT_I2C_MODE_COMPLETE_XFER);
         print_error_status(err);
         
+        sprintf(buffer,"Transferred bytes: %lu \r\n",I2C_PORT_I2CMasterGetWriteBufSize());
+        UART_PORT_UartPutString(buffer);
+        err = I2C_PORT_I2CMasterReadBuf(address, rd_buffer,2, I2C_PORT_I2C_MODE_COMPLETE_XFER);
+        sprintf(buffer,"Read value: %d %d \r\n",rd_buffer[0],rd_buffer[1]);
+        UART_PORT_UartPutString(buffer);
+        print_error_status(err);
         CyDelay(delay);
+        
         wr_buffer[1]= wr_buffer[1]+10;
         wr_buffer[2]= wr_buffer[2]+10;
     }
